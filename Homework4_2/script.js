4c6e8f99e7a99ddadaba9fb9e7b5b44bb45190ec
kyrylsherstobaiev <novemberua@gmail.com>
2021-02-02 23:14:15 +0200

    corr array fruit

--- Homework4_2/script.js
+++ Homework4_2/script.js
@@ -18,12 +18,12 @@ let summerSports = sports.slice(5, 12);
 
 let fruit = [...winterSports.splice(2, 1), ...summerSports.splice(2, 2)];
 
-console.log("***WinterSpots***")
+console.log("***WinterSports***")
 for (i = 0; i < winterSports.length; i++) {
 	console.log(winterSports[i].join(": "));
 }
 
-console.log("***SummerSpots***")
+console.log("***SummerSports***")
 for (i = 0; i < summerSports.length; i++) {
 	console.log(summerSports[i].join(": "));
 }
